892526948f17e413600ca0c9bc87d61a79a1ca5f
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-12-25 11:11:42 -0400

    Integrando el caso de uso de ortografía

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react';
-import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components";
+import { GptMessage, GptOrthographyMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components";
+import { orthographyUseCase } from '../../../core/use-cases';
 
 interface Message {
   text: string;
   isGpt: boolean;
+  info?: {
+    userScore: number;
+    errors: string[];
+    message: string;
+  }
 }
-
-
-
 
 export const OrthographyPage = () => {
 
@@ -20,16 +23,19 @@ export const OrthographyPage = () => {
     setIsLoading(true);
     setMessages( (prev) => [...prev, { text: text, isGpt: false }] );
 
-    //TODO: UseCase
+    const { ok, errors, message, userScore } = await orthographyUseCase(text);
+    if ( !ok ) {
+      setMessages( (prev) => [...prev, { text: 'No se pudo realizar la corrección', isGpt: true }] );
+    } else {
+      setMessages( (prev) => [...prev, { 
+        text: message, isGpt: true,  
+        info: {errors,message,userScore}
+      }]);
+    }
+    
     
     setIsLoading(false);
-
-    // Todo: Añadir el mensaje de isGPT en true
-
-
   }
-
-
 
   return (
     <div className="chat-container">
@@ -42,7 +48,10 @@ export const OrthographyPage = () => {
             messages.map( (message, index) => (
               message.isGpt
                 ? (
-                  <GptMessage key={ index } text="Esto es de OpenAI" />
+                  <GptOrthographyMessage 
+                    key={ index }  
+                    { ...message.info! }
+                  />
                 )
                 : (
                   <MyMessage key={ index } text={ message.text } />
